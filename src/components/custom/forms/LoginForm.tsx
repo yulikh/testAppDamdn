cb4719512e46593ebbfea69d5a58b86386e2c91c
yulikh <yuliankhabal@gmail.com>
2025-02-24 13:49:45 +0200

    added amplify.yml

--- src/components/custom/forms/LoginForm.tsx
+++ src/components/custom/forms/LoginForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -7,7 +6,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
